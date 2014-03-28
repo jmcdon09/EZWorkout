cf20847e127268865311ba6cea29166a7e922176
Jeremy McDonald <jeremymcdon@gmail.com>
2014-03-27 22:28:33 -0400

    Design Workout

--- EzTrain/EzTrain/src/cisc/teamnine/eztrain/PreviewExercise.java
+++ EzTrain/EzTrain/src/cisc/teamnine/eztrain/PreviewExercise.java
@@ -1,16 +1,34 @@
 package cisc.teamnine.eztrain;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class PreviewExercise extends Activity {
 
-	String exercise; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Bundle get_info = getIntent().getExtras();
 		super.onCreate(savedInstanceState);
+		
+		String exercise = get_info.getString("EXERCISE_NAME");
 		setContentView(R.layout.activity_preview_exercise);
+		
+		final TextView exercise_name = (TextView) findViewById(R.id.preview_exercise_name);
+		exercise_name.setText(exercise);
+		
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+
+
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
 	}
 
 	@Override
@@ -18,6 +36,23 @@ public class PreviewExercise extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.preview_exercise, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
